6c15dd2ae58ac8c88cc1afbb887878e62fc777a5
Jordi <jorrom16@gmail.com>
2026-02-25 14:09:09 +0000

    Navbar Changes - Made minor CSS changes - Added Gontserrat font family to the project - Set Gontserrat as the main font for the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/fonts/Gontserrat/Gontserrat-Regular.ttf";
+import "./assets/fonts/Gontserrat/Gontserrat-Italic.ttf";
+import "./assets/fonts/Gontserrat/Gontserrat-Bold.ttf";
+import "./assets/fonts/Gontserrat/Gontserrat-BoldItalic.ttf";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
