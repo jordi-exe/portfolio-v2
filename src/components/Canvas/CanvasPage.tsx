975920f06ca866f593f24a64e923ce304002449e
Jordi <jorrom16@gmail.com>
2026-08-03 17:58:21 +0100

    Removed unused useRef from CanvasPage.tsx

--- src/components/Canvas/CanvasPage.tsx
+++ src/components/Canvas/CanvasPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useTransition, animated, useSpringRef } from "@react-spring/web";
 import type { CSSProperties } from "react";
 import type { AnimatedProps } from "@react-spring/web";
